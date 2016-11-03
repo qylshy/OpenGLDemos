de16434dc9ae52a12a812080816932784702df32
qiuyunlong <qiuyunlong@oneniceapp.com>
2016-11-03 12:03:53 +0800

    glsl

---
+++ app/src/main/java/qyl/com/opengldemos/es2guide/FirstOpenGLProjectRenderer.java
@@ -0,0 +1,37 @@
+package qyl.com.opengldemos.es2guide;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glViewport;
+
+/**
+ * Created by qiuyunlong on 16/10/27.
+ */
+
+public class FirstOpenGLProjectRenderer  implements GLSurfaceView.Renderer{
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        // Set the OpenGL viewport to fill the entire surface.
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        // Clear the rendering surface.
+        glClear(GL_COLOR_BUFFER_BIT);
+
+    }
+}
---
+++ app/src/main/java/qyl/com/opengldemos/es2guide/util/LoggerConfig.java
@@ -0,0 +1,9 @@
+package qyl.com.opengldemos.es2guide.util;
+
+/**
+ * Created by qiuyunlong on 16/11/2.
+ */
+
+public class LoggerConfig {
+    public static final boolean ON = true;
+}
