c738e83886060f6073f5d9b6a03d0454fb277e38
qiuyunlong <qiuyunlong@oneniceapp.com>
2016-11-30 17:57:13 +0800

    AirHockeyWithImprovedMallets

--- app/src/main/java/qyl/com/opengldemos/es2guide/FirstOpenGLProjectActivity.java
+++ app/src/main/java/qyl/com/opengldemos/es2guide/FirstOpenGLProjectActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 import qyl.com.opengldemos.es2guide.render.AirHockeyRender2;
@@ -34,16 +36,53 @@ public class FirstOpenGLProjectActivity extends Activity {
         final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
         final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
 
+        final AirHockeyRender3 airHockeyRenderer = new AirHockeyRender3(this);
+
         if (supportsEs2) {
             // Request an OpenGL ES 2.0 compatible context. glSurfaceView.setEGLContextClientVersion(2);
             // Assign our renderer.
             glSurfaceView.setEGLContextClientVersion(2);
-            glSurfaceView.setRenderer(new AirHockeyRender3(this));
+            glSurfaceView.setRenderer(airHockeyRenderer);
             rendererSet = true;
         } else {
             Toast.makeText(this, "This device does not support OpenGL ES 2.0.", Toast.LENGTH_LONG).show();
             return;
         }
+
+        glSurfaceView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event != null) {
+                    // Convert touch coordinates into normalized device
+                    // coordinates, keeping in mind that Android's Y
+                    // coordinates are inverted.
+                    final float normalizedX =
+                            (event.getX() / (float) v.getWidth()) * 2 - 1;
+                    final float normalizedY =
+                            -((event.getY() / (float) v.getHeight()) * 2 - 1);
+
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        glSurfaceView.queueEvent(new Runnable() {
+                            @Override
+                            public void run() {
+                                airHockeyRenderer.handleTouchPress(normalizedX, normalizedY);
+                            }
+                        });
+                    }else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                        glSurfaceView.queueEvent(new Runnable() {
+                            @Override
+                            public void run() {
+                                airHockeyRenderer.handleTouchDrag(normalizedX, normalizedY);
+                            }
+                        });
+                    }
+                    return true;
+                }else {
+                    return false;
+                }
+
+            }
+        });
 
     }
 
--- app/src/main/java/qyl/com/opengldemos/es2guide/render/AirHockeyRender3.java
+++ app/src/main/java/qyl/com/opengldemos/es2guide/render/AirHockeyRender3.java
@@ -12,6 +12,8 @@ import qyl.com.opengldemos.es2guide.objects.Puck;
 import qyl.com.opengldemos.es2guide.objects.Table;
 import qyl.com.opengldemos.es2guide.progarms.ColorShaderProgram;
 import qyl.com.opengldemos.es2guide.progarms.TextureShaderProgarm;
+import qyl.com.opengldemos.es2guide.util.Geometry;
+import qyl.com.opengldemos.es2guide.util.Geometry.*;
 import qyl.com.opengldemos.es2guide.util.MatrixHelper;
 import qyl.com.opengldemos.es2guide.util.TextureHelper;
 
@@ -19,7 +21,9 @@ import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glViewport;
+import static android.opengl.Matrix.invertM;
 import static android.opengl.Matrix.multiplyMM;
+import static android.opengl.Matrix.multiplyMV;
 import static android.opengl.Matrix.rotateM;
 import static android.opengl.Matrix.setIdentityM;
 import static android.opengl.Matrix.setLookAtM;
@@ -38,6 +42,7 @@ public class AirHockeyRender3 implements GLSurfaceView.Renderer {
     private final float[] viewMatrix = new float[16];
     private final float[] viewProjectionMatrix = new float[16];
     private final float[] modelViewProjectionMatrix = new float[16];
+    private final float[] invertedViewProjectionMatrix = new float[16];
 
     private Table table;
     private Mallet mallet;
@@ -47,6 +52,19 @@ public class AirHockeyRender3 implements GLSurfaceView.Renderer {
     private ColorShaderProgram colorProgram;
 
     private int texture;
+
+    private boolean malletPressed = false;
+    private Point blueMalletPosition;
+
+    private final float leftBound = -0.5f;
+    private final float rightBound = 0.5f;
+    private final float farBound = -0.8f;
+    private final float nearBound = 0.8f;
+
+    private Point previousBlueMalletPosition;
+
+    private Point puckPosition;
+    private Vector puckVector;
 
     public AirHockeyRender3(Context context) {
         this.context = context;
@@ -59,6 +77,10 @@ public class AirHockeyRender3 implements GLSurfaceView.Renderer {
         table = new Table();
         mallet = new Mallet(0.08f, 0.15f, 32);
         puck = new Puck(0.06f, 0.02f, 32);
+
+        blueMalletPosition = new Point(0f, mallet.height / 2f, 0.4f);
+        puckPosition = new Point(0f, puck.height / 2f, 0f);
+        puckVector = new Vector(0f, 0f, 0f);
 
         textureProgram = new TextureShaderProgarm(context);
         colorProgram = new ColorShaderProgram(context);
@@ -81,8 +103,35 @@ public class AirHockeyRender3 implements GLSurfaceView.Renderer {
         // Clear the rendering surface.
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Multiply the view and projection matrices together.
-        multiplyMM(viewProjectionMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+        // Translate the puck by its vector
+        puckPosition = puckPosition.translate(puckVector);
+
+        // If the puck struck a side, reflect it off that side.
+        if (puckPosition.x < leftBound + puck.radius
+                || puckPosition.x > rightBound - puck.radius) {
+            puckVector = new Vector(-puckVector.x, puckVector.y, puckVector.z);
+            puckVector = puckVector.scale(0.9f);
+        }
+        if (puckPosition.z < farBound + puck.radius
+                || puckPosition.z > nearBound - puck.radius) {
+            puckVector = new Vector(puckVector.x, puckVector.y, -puckVector.z);
+            puckVector = puckVector.scale(0.9f);
+        }
+        // Clamp the puck position.
+        puckPosition = new Point(
+                clamp(puckPosition.x, leftBound + puck.radius, rightBound - puck.radius),
+                puckPosition.y,
+                clamp(puckPosition.z, farBound + puck.radius, nearBound - puck.radius)
+        );
+
+        // Friction factor
+        puckVector = puckVector.scale(0.99f);
+
+        // Update the viewProjection matrix, and create an inverted matrix for
+        // touch picking.
+        multiplyMM(viewProjectionMatrix, 0, projectionMatrix, 0,
+                viewMatrix, 0);
+        invertM(invertedViewProjectionMatrix, 0, viewProjectionMatrix, 0);
 
         // Draw the table.
         positionTableInScene();
@@ -98,7 +147,8 @@ public class AirHockeyRender3 implements GLSurfaceView.Renderer {
         mallet.bindData(colorProgram);
         mallet.draw();
 
-        positionObjectInScene(0f, mallet.height / 2f, 0.4f);
+        positionObjectInScene(blueMalletPosition.x, blueMalletPosition.y,
+                blueMalletPosition.z);
         colorProgram.setUniforms(modelViewProjectionMatrix, 0f, 0f, 1f);
         // Note that we don't have to define the object data twice -- we just
         // draw the same mallet again but in a different position and with a
@@ -106,7 +156,7 @@ public class AirHockeyRender3 implements GLSurfaceView.Renderer {
         mallet.draw();
 
         // Draw the puck.
-        positionObjectInScene(0f, puck.height / 2f, 0f);
+        positionObjectInScene(puckPosition.x, puckPosition.y, puckPosition.z);
         colorProgram.setUniforms(modelViewProjectionMatrix, 0.8f, 0.8f, 1f);
         puck.bindData(colorProgram);
         puck.draw();
@@ -128,5 +178,89 @@ public class AirHockeyRender3 implements GLSurfaceView.Renderer {
         multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix,
                 0, modelMatrix, 0);
     }
+
+    public void handleTouchPress(float normalizedX, float normalizedY) {
+        Ray ray = convertNormalized2DPointToRay(normalizedX, normalizedY);
+
+        // Now test if this ray intersects with the mallet by creating a
+        // bounding sphere that wraps the mallet.
+        Sphere malletBoundingSphere = new Sphere(new Point(
+                blueMalletPosition.x,
+                blueMalletPosition.y,
+                blueMalletPosition.z),
+                mallet.height / 2f);
+        malletPressed = Geometry.intersects(malletBoundingSphere, ray);
+    }
+
+    public void handleTouchDrag(float normalizedX, float normalizedY) {
+        if (malletPressed) {
+            Ray ray = convertNormalized2DPointToRay(normalizedX, normalizedY);
+            // Define a plane representing our air hockey table.
+            Plane plane = new Plane(new Point(0, 0, 0), new Vector(0, 1, 0));
+            // Find out where the touched point intersects the plane
+            // representing our table. We'll move the mallet along this plane.
+            Point touchedPoint = Geometry.intersectionPoint(ray, plane);
+            // Clamp to bounds
+
+            previousBlueMalletPosition = blueMalletPosition;
+            /*
+            blueMalletPosition =
+                new Point(touchedPoint.x, mallet.height / 2f, touchedPoint.z);
+            */
+            // Clamp to bounds
+            blueMalletPosition = new Point(
+                    clamp(touchedPoint.x,
+                            leftBound + mallet.radius,
+                            rightBound - mallet.radius),
+                    mallet.height / 2f,
+                    clamp(touchedPoint.z,
+                            0f + mallet.radius,
+                            nearBound - mallet.radius));
+
+            // Now test if mallet has struck the puck.
+            float distance =
+                    Geometry.vectorBetween(blueMalletPosition, puckPosition).length();
+
+            if (distance < (puck.radius + mallet.radius)) {
+                // The mallet has struck the puck. Now send the puck flying
+                // based on the mallet velocity.
+                puckVector = Geometry.vectorBetween(
+                        previousBlueMalletPosition, blueMalletPosition);
+            }
+        }
+    }
+
+
+    private void divideByW(float[] vertor) {
+        vertor[0] /= vertor[3];
+        vertor[1] /= vertor[3];
+        vertor[2] /= vertor[3];
+
+    }
+
+    private Ray convertNormalized2DPointToRay(float normalizedX, float normalizeY) {
+        final float[] nearPointNdc = {normalizedX, normalizeY, -1, 1};
+        final float[] farPointNdc = {normalizedX, normalizeY, 1, 1};
+
+        final float[] nearPointWorld = new float[4];
+        final float[] farPointWorld = new float[4];
+
+        multiplyMV(nearPointWorld, 0, invertedViewProjectionMatrix, 0, nearPointNdc, 0);
+        multiplyMV(farPointWorld, 0, invertedViewProjectionMatrix, 0, farPointNdc, 0);
+
+        divideByW(nearPointWorld);
+        divideByW(farPointWorld);
+
+        Point nearPointRay = new Point(nearPointWorld[0], nearPointWorld[1], nearPointWorld[2]);
+
+        Point farPointRay = new Point(farPointWorld[0], farPointWorld[1], farPointWorld[2]);
+
+        return new Ray(nearPointRay, Geometry.vectorBetween(nearPointRay, farPointRay));
+    }
+
+    private float clamp(float value, float min, float max) {
+        return Math.min(max, Math.max(value, min));
+    }
+
 
 }
